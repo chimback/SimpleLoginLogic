2712ca6888b04caee4c7aa7e61ef5c21b1987eec
chimback <77915239+chimback@users.noreply.github.com>
2021-04-25 14:30:45 +0900

    Update views.py

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,40 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .forms import SignupForm
 from django.http.response import HttpResponseRedirect
 from django.urls.base import reverse
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login, authenticate
 
-def base(request): #Home
-    return render(request, 'base.html')
+from .forms import SignupForm
 
-def signup(request): #SignUp
-    if request.method == "GET":
-        return render(request, 'signup.html', {'f':SignupForm()})
-    elif request.method == "POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            if form.cleaned_data['password'] == form.cleaned_data['password_check']:
-                new_user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
-                new_user.last_name = form.cleaned_data['last_name']
-                new_user.first_name = form.cleaned_data['first_name']
-                new_user.save()
-                return HttpResponseRedirect(reverse('login'))
-            else:
-                return render(request,'signup.html',{'f':form, 'error':'Password and password check values are different'})
-        else:
-            return render(request, 'signup.html', {'f':form})
 
-def signIn(request): #Login
-    if request.method == "GET":
-        return render(request, 'login.html')
-    elif request.method == "POST":
-        id = request.POST['username']
-        pw = request.POST['password']
-        print(id,pw)
-        u = authenticate(username=id, password=pw)
-        if u:
-            login(request,user=u)
-            return HttpResponseRedirect(reverse('home'))
-        else:
-            return render(request,'login.html',{'error':'Wrong Id or Password'})
+def get_index(request): #Home
+    return render(request, "base.html")
+
+
+def get_signup(request):
+     return render(request, 'signup.html', {'f':SignupForm()})
+  
+def post_signup(request): #SignUp
+    form = SignupForm(request.POST)
+    if form.is_valid():
+        # 안에 로직들은 SignupForm
+        form.create_user()
+    return HttpResponseRedirect(reverse('login'))
+    
+
+def get_signin(request):
+    return render(request, 'login.html')
+
+def post_signin(request):
+    validated_data = SignInForms(request.data) 
+    authenticated_user = authenticate(**validated_data)
+    if authenticated_user:
+        login(request,user=authenticated_user)
+        return HttpResponseRedirect(reverse('home'))
+    return render(request,'login.html',{'error':'Wrong Id or Password'})
+
